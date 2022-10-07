f3d086af79a88140fab223182480940a814623b7
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-10-07 16:37:50 +0530

    edits to expressjs committed

--- module-5-express/app.js
+++ module-5-express/app.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next)=> {
+    console.log("first")
+    next(); // passes control to next middleware
+})
+
+app.use((req, res, next)=> {
+    console.log("second")
 
 })
 
