2b5b7a6dc41acf4b54d130a6179287ce0303be98
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-10-08 09:17:32 +0530

    parsing incoming requests

--- module-5-express/app.js
+++ module-5-express/app.js
@@ -1,25 +1,36 @@
 const http= require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/',(req, res, next)=> {
-    console.log("always runs")
-    next(); // passes control to next middleware
-})
+app.use(bodyParser.urlencoded({extended: true}));
+
+// app.use('/',(req, res, next)=> {
+//     console.log("always runs")
+//     next(); // passes control to next middleware
+// })
 
 app.use('/extra-route',(req, res, next)=> {
-    console.log("second")
 
-    res.send("<html>hey</html>")
-    next();
+
+    res.send('<html><form action="/product" method="POST">hey<input type="text" name="title"> <button type="submit">submit</button></form></html>')
+
 })
 
+app.use('/product', (req, res,next)=> {
+    
+    console.log(req.body);
+    
+    res.redirect('/');
+
+})
 
 app.use('/',(req, res, next)=> {
     console.log("end")
 
     res.send("<html>hey</html>")
+    next();
 
 })
 
