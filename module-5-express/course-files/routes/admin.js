acacf32720722533dabb6e05929b93d70b11bf0b
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-10-10 23:19:34 +0530

    edits

--- module-5-express/course-files/routes/admin.js
+++ module-5-express/course-files/routes/admin.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path')
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
 
-  res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
   res.send(
     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
