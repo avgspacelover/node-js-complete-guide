693ae085f71113a4adc574d45e06f8e4909721e8
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-10-26 16:47:52 +0530

    edits

--- module-5-express/course-files/routes/admin.js
+++ module-5-express/course-files/routes/admin.js
@@ -4,6 +4,10 @@ const express = require('express');
 const rootDir = require('../util/path')
 const router = express.Router();
 
+const products = [];
+
+
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
 
@@ -16,7 +20,9 @@ router.get('/add-product', (req, res, next) => {
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
+  products.push( {title: req.body.title})
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products= products;
